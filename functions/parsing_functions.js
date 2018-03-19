0b5b870d3677e76acb6baac08a3b6425446b9f58
Kazuhiro <samuel.radat@epitech.eu>
2018-03-19 17:27:40 +0100

    xp system

--- functions/parsing_functions.js
+++ functions/parsing_functions.js
@@ -1,5 +1,19 @@
 const bot_data = require("../bot_data");
+const RichEmbed = require("discord.js").RichEmbed;
+
+class MemberUserXP {
+    constructor(id) {
+        this.id = id;
+        this.multiplicateur = 1;
+        this.xp = 0;
+        this.level = 0;
+    }
+
+}
+
 module.exports = {
+
+    MemberUserXP,
 
     check_message: function (message) {
 
@@ -23,6 +37,57 @@ module.exports = {
 
             }
         }
+
+    },
+
+    check_xp: (client, message) => {
+        let memberXPData = client.memberXP.get(message.author.id);
+
+        if (!memberXPData) {
+            client.memberXP.set(message.author.id, new MemberUserXP(message.author.id));
+            memberXPData = client.memberXP.get(message.author.id);
+        }
+
+
+        let palier_reached = memberXPData.level;
+
+        memberXPData.xp += message.content.length / 10;
+
+        Object.keys(bot_data.xp_table).forEach(palier => {
+
+            if (memberXPData.xp > bot_data.xp_table[palier].xp)
+                palier_reached = palier;
+
+        });
+
+        if (palier_reached > memberXPData.level) {
+            memberXPData.level += 1;
+
+            message.channel.send(new RichEmbed()
+                .addField(
+                    `**${message.member.displayName}** est passé à la **Division ${bot_data.xp_table[memberXPData.level].string}**`,
+                    `*${bot_data.xp_table[memberXPData.level].description}*`)
+                .setColor(message.guild.me.displayColor)
+            ).catch(console.error);
+
+            message.guild.roles.array().forEach(role => {
+
+                if (role.name.split(' ')[1] === bot_data.xp_table[memberXPData.level].string) {
+                    if (message.member) {
+                        message.member.addRole(role).catch(err => {
+                            console.error(err);
+                            message.channel.send("Impossible d'ajouter le rôle correspondant.").catch(console.error);
+                        });
+                    } else {
+                        message.channel.send("Impossible d'ajouter le rôle correspondant.").catch(console.error);
+                    }
+                }
+
+            });
+
+        }
+
+        client.memberXP.set(message.author.id, memberXPData);
 
     },
 
---
+++ help/ranking.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    help_msg: "Permet d'afficher le classement des membres en fonction des fragments d'espoir."
+
+};
