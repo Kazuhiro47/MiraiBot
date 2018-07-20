35ab14e6a1eab918d16c7c715b9a66ba13d8a3fd
Kazuhiro <samuel.radat@epitech.eu>
2018-07-20 11:31:25 +0200

    twitter tweet parsing

--- functions/twitter.js
+++ functions/twitter.js
@@ -1,25 +1,30 @@
 let Twitter = require('twitter');
 const bot_data = require("../bot_data");
+let RichEmbed = require("discord.js").RichEmbed;
 
-let client = new Twitter({
-    consumer_key: bot_data.twitter_api.consumer_key,
-    consumer_secret: bot_data.twitter_api.consumer_secret,
-    access_token_key: bot_data.twitter_api.access_token_key,
-    access_token_secret: bot_data.twitter_api.access_token_secret
-});
+let initTwitterListener = (channel, kazuhiro) => {
 
-/**
- * Stream statuses filtered by keyword
- * number of tweets per second depends on topic popularity
- **/
-client.stream('statuses/filter', {track: 'danganronpa'},  function(stream) {
-    stream.on('data', function(tweet) {
-        if (!tweet.text.startsWith("RT")) {
-            console.log(tweet.text + "\n--------------------\n");
-        }
+    let client = new Twitter({
+        consumer_key: bot_data.twitter_api.consumer_key,
+        consumer_secret: bot_data.twitter_api.consumer_secret,
+        access_token_key: bot_data.twitter_api.access_token_key,
+        access_token_secret: bot_data.twitter_api.access_token_secret
     });
 
-    stream.on('error', function(error) {
-        console.log(error);
+    client.stream('statuses/filter', {track: 'danganronpa'},  function(stream) {
+        stream.on('data', function(tweet) {
+            if (tweet.user.screen_name === "danganronpawiki") {
+                channel.send(new RichEmbed().setAuthor(tweet.user.name, tweet.user.profile_image_url)
+                    .setDescription(tweet.text)
+                    .setFooter(tweet.created_at)).catch(console.error);
+            }
+        });
+
+        stream.on('error', function(error) {
+            console.log(error);
+        });
     });
-});
+
+};
+
+module.exports = {initTwitterListener};
