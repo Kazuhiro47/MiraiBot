cd9fa3b509dd6950c212b662f2eadaa2b620cfa5
Kazuhiro <samuel.radat@epitech.eu>
2018-07-20 11:31:11 +0200

    ping cmd update

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,14 @@
+let bot_data = require("../bot_data.js").bot_values;
+
 exports.run = (client, message) => {
 
 	console.log('Launching ping command.');
     message.channel.send({embed: {
-        color: message.guild.me.displayColor,
+        color: bot_data.bot_color,
         fields: [{
             name: "Pong !",
             value: `:ping_pong: ${Math.round(client.ping)}ms`
         }]
-    }});
+    }}).catch(console.error);
 
 };
