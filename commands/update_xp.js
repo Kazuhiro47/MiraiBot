220a3ddc1d78ae882aa758417fd44dd964bb49a7
Kazuhiro <samuel.radat@epitech.eu>
2018-07-20 11:32:00 +0200

    files renamed and xp management update

--- commands/update_xp.js
+++ commands/update_xp.js
@@ -11,142 +11,134 @@ exports.run = (client, message, args) => {
 
     function analyse_channel(chan, args) {
 
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
 
             let msg_collected = 42;
 
             try {
-                chan.fetchMessages({limit:1}).catch(console.error);
+                await chan.fetchMessages({limit: 1});
             } catch (e) {
                 console.error(`${chan.type}, ${chan.id}`);
                 resolve(true);
             }
-            chan.fetchMessages({limit: 1}).then(messages => {
-                let message_id = messages.first().id;
+            let messages = await chan.fetchMessages({limit: 1});
+            let message_id = messages.first().id;
+            let count = 0;
 
-                let count = 0;
+            async function retrieve_message(id) {
 
-                function retrieve_message(id) {
+                let messages = await chan.fetchMessages({limit: 100, before: id});
 
-                    chan.fetchMessages({
-                        limit: 100,
-                        before: id
-                    }).then(messages => {
+                if (messages.array().length === 0) {
+                    console.log(`${chan.name} analysed`);
+                    return resolve(chan);
+                }
 
-                        if (messages.array().length === 0) {
-                            console.log(`${chan.name} analysed`);
-                            return resolve(chan);
-                        }
-                        if (!messages.last()) {
-                            console.error(messages.length);
-                            console.error("COLLECTION = " + messages);
-                        }
-                        message_id = messages.last().id;
-                        messages.array().forEach(function (msg_obj) {
+                if (!messages.last()) {
+                    console.error(messages.length);
+                    console.error("COLLECTION = " + messages);
+                }
+                message_id = messages.last().id;
 
-                            if (!msg_obj.member) {
+                messages.array().forEach(function (msg_obj) {
+
+                    if (!msg_obj.member) {
+                        return;
+                    }
+
+                    if (args.length !== 0) {
+
+                        let members = message.guild.members.keyArray();
+                        let member;
+                        let target = null;
+
+                        members.forEach(userId => {
+
+                            member = message.guild.members.find("id", userId);
+
+                            if (member.nickname) {
+                                if (member.nickname.toLowerCase().trim().includes(args.join(" ").toLowerCase().trim())) {
+                                    target = member;
+                                }
+                            }
+                            if (member.user.username.toLowerCase().trim().includes(args.join(" ").toLowerCase().trim())) {
+                                target = member;
+                            }
+                        });
+
+                        if (target === null) {
+                            message.channel.send("Utilisateur introuvable").catch(console.error);
+                            return reject(null);
+                        } else {
+
+                            if (target.id !== msg_obj.author.id) {
                                 return;
                             }
 
-                            if (args.length !== 0) {
+                        }
 
-                                let members = message.guild.members.keyArray();
-                                let member;
-                                let target = null;
+                    }
 
-                                members.forEach(userId => {
+                    let memberXPData = client.memberXP.get(msg_obj.author.id);
 
-                                    member = message.guild.members.find("id", userId);
+                    if (!memberXPData) {
+                        client.memberXP.set(msg_obj.author.id, new MemberUserXP(msg_obj.author.id));
+                        memberXPData = client.memberXP.get(msg_obj.author.id);
+                    }
 
-                                    if (member.nickname) {
-                                        if (member.nickname.toLowerCase().trim().includes(args.join(" ").toLowerCase().trim())) {
-                                            target = member;
-                                        }
-                                    }
-                                    if (member.user.username.toLowerCase().trim().includes(args.join(" ").toLowerCase().trim())) {
-                                        target = member;
-                                    }
-                                });
+                    let palier_reached = memberXPData.level;
 
-                                if (target === null) {
-                                    message.channel.send("Utilisateur introuvable").catch(console.error);
-                                    return reject(null);
-                                } else {
+                    memberXPData.xp += msg_obj.content.length / 10;
 
-                                    if (target.id !== msg_obj.author.id) {
-                                        return;
-                                    }
+                    Object.keys(bot_data.xp_table).forEach(palier => {
 
-                                }
-
-                            }
-
-                            let memberXPData = client.memberXP.get(msg_obj.author.id);
-
-                            if (!memberXPData) {
-                                client.memberXP.set(msg_obj.author.id, new MemberUserXP(msg_obj.author.id));
-                                memberXPData = client.memberXP.get(msg_obj.author.id);
-                            }
-
-                            let palier_reached = memberXPData.level;
-
-                            memberXPData.xp += msg_obj.content.length / 10;
-
-                            Object.keys(bot_data.xp_table).forEach(palier => {
-
-                                if (memberXPData.xp > bot_data.xp_table[palier].xp)
-                                    palier_reached = palier;
-
-                            });
-
-                            if (palier_reached > memberXPData.level) {
-                                memberXPData.level += 1;
-
-                                message.channel.send(new RichEmbed()
-                                    .addField(
-                                        `**${msg_obj.member.displayName}** est passé à la **Division ${bot_data.xp_table[memberXPData.level].string}**`,
-                                        `*${bot_data.xp_table[memberXPData.level].description}*`)
-                                    .setColor(message.guild.me.displayColor)
-                                ).catch(console.error);
-
-                                message.channel.guild.roles.array().forEach(role => {
-
-                                    if (role.name.split(' ')[1] === bot_data.xp_table[memberXPData.level].string) {
-                                        if (msg_obj.member) {
-                                            msg_obj.member.addRole(role).catch(err => {
-                                                console.error(err);
-                                                message.channel.send("Impossible d'ajouter le rôle correspondant.").catch(console.error);
-                                            });
-                                        } else {
-                                            message.channel.send("Impossible d'ajouter le rôle correspondant.").catch(console.error);
-                                        }
-                                    }
-
-                                });
-
-                            }
-
-                            client.memberXP.set(msg_obj.author.id, memberXPData);
-
-                            count += 1;
-                            if (count % 2000 === 0) console.log(`${chan.name}: Successfully analysed (${count}) messages. (${msg_obj.content})`);
-
-                        });
-
-                        msg_collected = messages;
-                        return (retrieve_message(message_id));
-
-                    }).catch((err) => {
-                        console.error(err);
+                        if (memberXPData.xp > bot_data.xp_table[palier].xp)
+                            palier_reached = palier;
 
                     });
 
-                }
+                    if (palier_reached > memberXPData.level) {
+                        memberXPData.level += 1;
 
-                retrieve_message(message_id);
+                        message.channel.send(new RichEmbed()
+                            .addField(
+                                `**${msg_obj.member.displayName}** est passé à la **Division ${bot_data.xp_table[memberXPData.level].string}**`,
+                                `*${bot_data.xp_table[memberXPData.level].description}*`)
+                            .setColor(message.guild.me.displayColor)
+                        ).catch(console.error);
+
+                        message.channel.guild.roles.array().forEach(role => {
+
+                            if (role.name.split(' ')[1] === bot_data.xp_table[memberXPData.level].string) {
+                                if (msg_obj.member) {
+                                    msg_obj.member.addRole(role).catch(err => {
+                                        console.error(err);
+                                        message.channel.send("Impossible d'ajouter le rôle correspondant.").catch(console.error);
+                                    });
+                                } else {
+                                    message.channel.send("Impossible d'ajouter le rôle correspondant.").catch(console.error);
+                                }
+                            }
+
+                        });
+
+                    }
+
+                    client.memberXP.set(msg_obj.author.id, memberXPData);
+
+                    count += 1;
+                    if (count % 500 === 0) console.log(`${chan.name}: Successfully analysed (${count}) messages. (${msg_obj.content})`);
+
+                });
+
+                msg_collected = messages;
+                return (retrieve_message(message_id));
 
 
-            }).catch(console.error);
+            }
+
+            retrieve_message(message_id).catch(console.error);
+
 
         });
 
@@ -155,9 +147,10 @@ exports.run = (client, message, args) => {
     let updateXP = async () => {
         let channels = message.guild.channels.array();
 
-        for (let i = 0 ; i < channels.length ; i++) {
+        for (let i = 0; i < channels.length; i++) {
             await analyse_channel(channels[i], args);
         }
+        message.channel.send("Update done.").catch(console.error);
     };
 
     updateXP().then(() => {
--- help/ranking.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-
-    help_msg: "Permet d'afficher le classement des membres en fonction des fragments d'espoir."
-
-};
