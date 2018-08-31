07daf4b885d97623d270679fb2be8a443a816660
Kazuhiro <kazuhiro47@yahoo.fr>
2018-08-31 06:13:17 +0200

    mute commands

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,5 +1,6 @@
 const RichEmbed = require("discord.js").RichEmbed;
 const bot_data = require("../bot_data.js");
+const Mute = require("../functions/cmds/mute").Mute;
 const find_user = require("../functions/find_user.js").find_user;
 
 let unmute = (client, message, args) => new Promise((resolve, reject) => {
@@ -11,40 +12,12 @@ let unmute = (client, message, args) => new Promise((resolve, reject) => {
 
     if (moderatorUser.hasPermission('BAN_MEMBERS') || moderatorUser.hasPermission('ADMINISTRATOR')) {
 
-        let name = args.join(' ');
-        let target = find_user(client, name);
-
-        if (!target) {
-            message.channel.send(new RichEmbed()
-                .setColor(bot_data.bot_values.bot_color)
-                .setTitle("Erreur commande mute")
-                .addField("Utilisation", "/mute {pseudo}")
-            ).catch(console.error);
-            return;
-        }
-
         let promises = [];
-
-        message.guild.channels.array().forEach(channel => {
-
-            if (channel.type === "text") {
-                channel.permissionOverwrites.array().forEach(p => {
-                    if (p.id === target.id) {
-                        promises.push(p.delete());
-                    }
-                });
-            }
-
+        message.mentions.members.array().forEach(target => {
+            promises.push(new Mute(client, message, target).unmute());
         });
 
-        Promise.all(promises).then(() => {
-            message.channel.send(new RichEmbed()
-                .setColor(target.displayColor)
-                .setAuthor(target.displayName, target.user.avatarURL)
-                .setDescription("Unmuted")
-            ).catch(console.error);
-            resolve(true);
-        }).catch(err => reject(err));
+        Promise.all(promises).then(() => resolve(true)).catch(err => reject(err));
 
     } else {
         message.channel.send("Tu ne possèdes pas le pouvoir.").catch(console.error);
